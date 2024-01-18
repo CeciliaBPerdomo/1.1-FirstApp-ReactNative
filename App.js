4baf9db0275afbd47060b8aadd361d35f2078f8b
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-01-18 11:45:26 -0300

    1. Iniciando con React Native

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.hola}>¡Hola Mundo!</Text>
+      <Text>¡Qué emoción es mi primera app!</Text>
+      <Text style={styles.ahora}>¿Y ahora qué hago?</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ahora: {
+    fontSize: 25,
+    color: "red",
+  },
+  hola: {
+    fontSize: 35,
+    color: "red",
+    fontWeight: "bold"
+  }
 });
---
+++ readme.md
@@ -0,0 +1,7 @@
+## First app with React Native, men!
+- npx create-expo-app my-app
+- cd my-app
+- npm start
+
+## Cellular
+- expo app
